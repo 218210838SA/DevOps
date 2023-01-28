2217acad652a764884b2347a98e2e85d5354e970
Saad AlQahtani <218210838@psu.edu.sa>
2023-01-28 19:49:06 +0300

    With three print statements

---
+++ DEMO.java
@@ -0,0 +1,7 @@
+public class DEMO {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Devops");
+        System.out.println("Git is Fun");
+    }
+}

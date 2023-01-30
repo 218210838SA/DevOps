399d56e86e16ee7dfd4bf87efaa6174262198924
Saad AlQahtani <218210838@psu.edu.sa>
2023-01-30 08:42:39 +0300

    for loop added

--- demo222.java
+++ demo222.java
@@ -1,7 +1,8 @@
 public class DEMO {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java");
-        System.out.println("Welcome to Devops");
-        
+       for(int i=0;i<10;i++){
+				System.out.println("Welcome to Java");
+}
+
     }
 }

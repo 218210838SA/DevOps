c735e0966a6aa6150e4bad936a24af3a52718f48
Saad AlQahtani <218210838@psu.edu.sa>
2023-02-11 16:40:17 +0300

    Pushed For Real JenkinsWithGit.java

--- JenkinsWithGit.java
+++ JenkinsWithGit.java
@@ -9,7 +9,8 @@ public class JenkinsWithGits {
         //code to print unicode Characters
         int charPerLine =0;
         for (int i = 33; i < 256; i++, charPerLine++) {
-            System.out.print((char)i+((charPerLine%10==0)?"\n":"\t"));
+            System.out.print((char)i);
+				System.out.print((charPerLine%10==0)?"\n":"\t");
             System.out.print("");
         }
     }
